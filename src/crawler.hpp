bee694e5ddca71487997def82144f82ff3a9a796
Shiv Govil <shivgov@umich.edu>
2025-02-03 18:50:43 -0500

    ksjdhfkdsjfh

--- src/crawler.hpp
+++ src/crawler.hpp
@@ -1,19 +1,42 @@
 #include "lib/bloom_filter.hpp"
 #include "lib/mutex.hpp"
 
+static constexpr int THREAD_COUNT = 20;
+
 class crawler {
 public:
-  crawler() : ___ {
-
-  }
+  /*crawler() {*/
+  /**/
+  /*}*/
 
   void run() {
+    for (auto &t : thread_pool) {
+      if (pthread_create(&t, nullptr, worker, nullptr)) {
+        throw std::runtime_error("pthread creation failed\n");
+      }
+    }
 
+    for (auto &t: thread_pool) {
+      pthread_join(t, nullptr);
+    }
   }
 
-  void worker();
 private:
-  fast::bloom_filter visited_urls;
-  // frontier crawl_frontier
+  // fast::bloom_filter visited_urls;
+  // frontier crawl_frontier;
+  pthread_t thread_pool[THREAD_COUNT];
+  fast::mutex bloom_mutex;
+  fast::mutex frontier_mutex;
 
+  static void* worker(void *arg) {
+    /* 
+     * frontier_mutex.lock();
+     * auto link = frontier.next();
+     * frontier_mutex.unlock();
+     *
+     * bloom_mutex.lock();
+     * if (visited_urls.contains(link)) { continue; }
+     */
+
+  }
 };
