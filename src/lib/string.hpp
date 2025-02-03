b871d54b586cbb058d3b343a4e3fd40ad564bb63
Jim Xiong <xiong.jim131@gmail.com>
2025-02-03 18:50:41 -0500

    string

--- src/lib/string.hpp
+++ src/lib/string.hpp
@@ -9,20 +9,29 @@ class string {
   size_t cap;
   char* buffer;
 
-  public:
-  size_t size() const { return len; }
-
-  char& operator[](size_t idx) const {
-    return buffer[idx];
+  void grow(size_t new_len) {
+    // buffer = rea
   }
 
+ public:
+  string() : len(0), cap(8), buffer(new char[8]) {}
+
+  size_t size() const { return len; }
+
+  char& operator[](size_t idx) const { return buffer[idx]; }
+
   void operator+=(const string& str) {
-    if(len + str.len >= cap) {
-      grow(len + str.len + 1);
+    size_t new_len = len + str.len + 1;
+    if (new_len > cap) {
+      grow(new_len);
     }
 
-    for()
+    for (size_t i = 0; i < str.len; ++i) {
+      buffer[len + i] = str.buffer[i];
+    }
+
+    len = new_len;
   }
 };
 
-}
+}  // namespace fast
