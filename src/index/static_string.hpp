13f460a0b7be50664d4209ed0512d3c89da10d09
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-02-06 08:40:33 -0500

    static string

---
+++ src/index/static_string.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstddef>
+
+namespace fast {
+class static_string {
+  char *buffer_;
+  size_t len_;
+
+  public:
+  static_string(const char *start, const char *end);
+
+  bool operator==(const static_string &other) {
+    if (len_ != other.len_) return false;
+
+    for (auto i = 0ul; i < len_; ++i)
+      if (buffer_[i] != other.buffer_[i]) return false;
+
+    return true;
+  }
+};
+}
