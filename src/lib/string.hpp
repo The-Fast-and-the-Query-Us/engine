3aa1b2fb78bf5d9f3815ac7b0fe31fa745647851
Jake Zur <jakezur@umich.edu>
2025-02-10 16:47:28 -0500

    added equals operators

--- src/lib/string.hpp
+++ src/lib/string.hpp
@@ -9,20 +9,20 @@ namespace fast {
 class string {
   size_t len;
   size_t cap;
-  char* buffer;
+  char *buffer;
 
   void grow(size_t need) {
-    buffer = (char*) realloc(buffer, need + 1);
+    buffer = (char *)realloc(buffer, need + 1);
     cap = need;
   }
 
- public:
-  string() : len(0), cap(7), buffer((char*) malloc(8)) {}
+public:
+  string() : len(0), cap(7), buffer((char *)malloc(8)) {}
 
-  string(const string& other) {
+  string(const string &other) {
     len = other.len;
     cap = other.len;
-    buffer = (char*) malloc(cap + 1);
+    buffer = (char *)malloc(cap + 1);
 
     memcpy(buffer, other.buffer, len);
   }
@@ -30,7 +30,7 @@ class string {
   string(const char *str) {
     len = strlen(str);
     cap = strlen(str);
-    buffer = (char*) malloc(cap + 1);
+    buffer = (char *)malloc(cap + 1);
 
     memcpy(buffer, str, len);
   }
@@ -39,26 +39,43 @@ class string {
 
   size_t size() const { return len; }
 
-  char& operator[](size_t idx) const { return buffer[idx]; }
+  char &operator[](size_t idx) const { return buffer[idx]; }
 
   const char *c_str() const { return data(); }
 
-  char *data() const { 
+  char *data() const {
     buffer[len] = 0;
-    return buffer; 
+    return buffer;
   }
 
   void reserve(size_t need) {
-    if (need > cap) grow(need);
+    if (need > cap)
+      grow(need);
   }
 
+  bool operator==(string &s) {
+    if (s.size() != this->size())
+      return false;
+
+    for (size_t i = 0; i < s.size(); i++) {
+      if (s[i] != (*this)[i])
+        return false;
+    }
+
+    return true;
+  }
+
+  bool operator!=(string &s) { return !(*this == s); }
+
   void operator+=(char c) {
-    if (len == cap) grow(len << 1);
+    if (len == cap)
+      grow(len << 1);
     buffer[len++] = c;
   }
 
-  void operator+=(const string& other) {
-    if (len + other.len > cap) grow(len + other.len);
+  void operator+=(const string &other) {
+    if (len + other.len > cap)
+      grow(len + other.len);
     memcpy(buffer + len, other.buffer, other.len);
     len += other.len;
   }
@@ -70,4 +87,4 @@ class string {
   }
 };
 
-}
+} // namespace fast
