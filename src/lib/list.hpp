f532167abf68d6a8f0105a85fee35b58e0f5ad71
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-02-14 17:13:31 -0500

    comment ideas

--- src/lib/list.hpp
+++ src/lib/list.hpp
@@ -9,7 +9,7 @@ template <class T, size_t size=64>
 class list {
   // maybe use allign as for better cache performance
   struct node {
-    T arr[size];
+    T arr[size]; // use malloc to not call element constructors
     size_t len = 0; // we could optimize by using pointer to track len if not supporting iterator operations
     node* next = nullptr;
   };
