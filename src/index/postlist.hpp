e8047d3ccb288b797e76293fadb7c417f0855ca6
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-02-24 12:57:39 -0500

    structure

--- src/index/postlist.hpp
+++ src/index/postlist.hpp
@@ -1,15 +1,24 @@
 #include "list.hpp"
+#include <cmath>
 #include <cstddef>
 #include <hashtable.hpp>
 
 namespace fast {
+/*
+* Format
+* numOccurances
+* Sync table: < doc delta, post delta, offset in posttable> , len == sqrt(len(post))
+*   - Terminated by 0x0000 because stored as (doc, offset)
+* Post table: < doc delta <post deltas> >
+*   - Terminated by 0x00 (doc must be different)
+*/
 class postlist {
-
   public:
 
   static size_t size_needed(const list<hashtable::post> &posts) {
-    (void)posts;
-    return 0; // TODO
+    (void) posts;
+
+    return sizeof(size_t) + 0 + 0; // TODO
   }
 
   // write posting list and return its end
