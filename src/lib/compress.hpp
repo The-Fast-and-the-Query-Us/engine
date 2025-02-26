3ec4bc1df5b1214c9d406829fa6030494e5dcd8c
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-02-26 14:57:47 -0500

    yer

--- src/lib/compress.hpp
+++ src/lib/compress.hpp
@@ -53,48 +53,4 @@ inline const unsigned char *skip_encoded(const unsigned char *buffer) {
   return buffer + ((*buffer) >> 5) + 1;
 }
 
-/*
-* Return the number of bytes required to compress the given number
-*/
-template<class T>
-size_t compressed_size(T number) {
-  size_t ans{1};
-  number >>= 7;
-  while (number) number >>= 7, ++ans;
-  return ans;
-}
-
-/*
-* Write a number to a buffer with high bits coming first
-*/
-template<class T>
-char* compress(T number, char* buffer) {
-  for (auto byte = compressed_size(number) - 1; byte; --byte, ++buffer) {
-    *buffer = (number >> (7 * byte)) | 0x80;
-  }
-
-  *buffer = (number) & ~(0x80);
-
-  return buffer + 1;
-}
-
-/*
-* Read bytes writen by compress
-*/
-template <class T>
-char* expand(T& number, char* buffer) {
-  number = 0;
-  do {
-    number <<= 7;
-    number |= uint8_t(*buffer & ~(0x80));
-  } while (*(buffer++) & 0x80);
-  return buffer;
-}
-
-// skip to one after current compressed number
-inline char* skip_compressed(char *buffer) {
-  while (*(buffer++) & 0x80);
-  return buffer;
-}
-
 }
