7b48105ae88af1e4b2f02e63c6d57ec5cffb14df
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-18 11:39:14 -0400

    yer

--- src/index/postlist.hpp
+++ src/index/postlist.hpp
@@ -15,17 +15,17 @@ namespace fast {
 */
 class postlist {
   static constexpr size_t MIN_SYNC = 100;
+
   size_t word_count, sync_len;
 
   size_t *sync() { return &sync_len + 1; }
-  char   *posts() { return reinterpret_cast<char*>(sync() + sync_len); }
+  unsigned char   *posts() { return reinterpret_cast<unsigned char*>(sync() + sync_len); }
 
   public:
 
   static size_t size_needed(const list<uint64_t> &posts) {
     const auto syncs = fast_sqrt(posts.size());
     size_t dynamic{0};
-
     if (syncs >= MIN_SYNC) {
       dynamic += 2 * sizeof(size_t) * syncs;
     }
@@ -40,7 +40,7 @@ class postlist {
     return dynamic + sizeof(postlist);
   }
 
-  static char *write(const list<uint64_t> &posts, postlist *buffer) {
+  static unsigned char *write(const list<uint64_t> &posts, postlist *buffer) {
     buffer->word_count = posts.size();
     const auto syncs = fast_sqrt(posts.size());
 
@@ -55,6 +55,12 @@ class postlist {
     size_t i    = 0;
 
     for (const auto post : posts) {
+      if (syncs >= MIN_SYNC && i % syncs == 0) {
+        buffer->sync()[i / syncs] = write_pos - buffer->posts();
+      }
+      write_pos = encode(post - last, write_pos);
+      last = post;
+      i++;
     }
 
     return write_pos;
