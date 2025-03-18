da9a248c3e18db1f62980ddb7f877386f21626b8
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-18 10:47:45 -0400

    layout postlist

--- src/index/postlist.hpp
+++ src/index/postlist.hpp
@@ -1,19 +1,64 @@
 #pragma once
 
 
+#include "common.hpp"
+#include "compress.hpp"
 #include "list.hpp"
+
 namespace fast {
 /*
 * in memory postlist serialization
 *  should we store number of documents?
+*
+*  Format:
+*  header, sync (uncompressed), posts (encoded)
 */
 class postlist {
-  size_t word_count;
+  static constexpr size_t MIN_SYNC = 100;
+  size_t word_count, sync_len;
+
+  size_t *sync() { return &sync_len + 1; }
+  char   *posts() { return reinterpret_cast<char*>(sync() + sync_len); }
+
   public:
 
-  static size_t size_needed(const list<uint64_t> &posts);
+  static size_t size_needed(const list<uint64_t> &posts) {
+    const auto syncs = fast_sqrt(posts.size());
+    size_t dynamic{0};
 
-  static char *write(const list<uint64_t> &posts, postlist *buffer);
+    if (syncs >= MIN_SYNC) {
+      dynamic += 2 * sizeof(size_t) * syncs;
+    }
+
+    size_t last = 0;
+    for (const auto post : posts) {
+      dynamic += encoded_size(post - last);
+      last = post;
+    }
+    dynamic += encoded_size(0);
+
+    return dynamic + sizeof(postlist);
+  }
+
+  static char *write(const list<uint64_t> &posts, postlist *buffer) {
+    buffer->word_count = posts.size();
+    const auto syncs = fast_sqrt(posts.size());
+
+    if (syncs >= MIN_SYNC) {
+      buffer->sync_len = syncs;
+    } else {
+      buffer->sync_len = 0;
+    }
+
+    auto write_pos = buffer->posts();
+    size_t last = 0;
+    size_t i    = 0;
+
+    for (const auto post : posts) {
+    }
+
+    return write_pos;
+  }
 };
 
 }
