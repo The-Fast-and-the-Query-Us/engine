0fef03ce4ea5ef0a35ef24f599e8f9b4b39a8f58
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-18 14:22:30 -0400

    postlist iteration

--- src/index/postlist.hpp
+++ src/index/postlist.hpp
@@ -8,24 +8,24 @@
 namespace fast {
 /*
 * in memory postlist serialization
-*  should we store number of documents?
 *
 *  Format:
-*  header, sync (uncompressed), posts (encoded)
+*  header, sync <post value, post offset>, posts <delta>\0
 */
 class postlist {
   static constexpr size_t MIN_SYNC = 100;
 
-  size_t word_count, sync_len;
+  size_t word_count, post_len, sync_count;
 
-  size_t *sync() { return &sync_len + 1; }
-  unsigned char   *posts() { return reinterpret_cast<unsigned char*>(sync() + sync_len); }
+  pair<size_t>  *sync()   { return reinterpret_cast<pair<size_t>*>(&sync_count + 1); }
+  unsigned char *posts()  { return reinterpret_cast<unsigned char*>(sync() + sync_count); }
 
   public:
 
   static size_t size_needed(const list<uint64_t> &posts) {
     const auto syncs = fast_sqrt(posts.size());
     size_t dynamic{0};
+
     if (syncs >= MIN_SYNC) {
       dynamic += 2 * sizeof(size_t) * syncs;
     }
@@ -35,6 +35,7 @@ class postlist {
       dynamic += encoded_size(post - last);
       last = post;
     }
+
     dynamic += encoded_size(0);
 
     return dynamic + sizeof(postlist);
@@ -45,9 +46,9 @@ class postlist {
     const auto syncs = fast_sqrt(posts.size());
 
     if (syncs >= MIN_SYNC) {
-      buffer->sync_len = syncs;
+      buffer->sync_count = syncs;
     } else {
-      buffer->sync_len = 0;
+      buffer->sync_count = 0;
     }
 
     auto write_pos = buffer->posts();
@@ -56,15 +57,53 @@ class postlist {
 
     for (const auto post : posts) {
       if (syncs >= MIN_SYNC && i % syncs == 0) {
-        buffer->sync()[i / syncs] = write_pos - buffer->posts();
+        buffer->sync()[i / syncs] = {size_t(write_pos - buffer->posts()), post};
       }
       write_pos = encode(post - last, write_pos);
       last = post;
       i++;
     }
 
+    write_pos = encode(0, write_pos);
+
+    buffer->post_len = write_pos - buffer->posts();
     return write_pos;
   }
+
+  class isr {
+    friend class postlist;
+
+    const unsigned char *buf;
+    uint64_t acc;
+
+    public:
+    
+    isr& operator++() {
+      uint64_t adj;
+      buf = decode(adj, buf);
+      acc += adj;
+      return *this;
+    }
+
+    uint64_t operator*() const {return acc;}
+
+    bool operator==(const isr &other) const {
+      return buf == other.buf;
+    }
+  };
+
+  isr begin() {
+    isr ans;
+    ans.buf = decode(ans.acc, posts());
+    return ans;
+  }
+
+  isr end() {
+    isr ans;
+    ans.buf = posts() + post_len;
+    return ans;
+  }
+
 };
 
 }
