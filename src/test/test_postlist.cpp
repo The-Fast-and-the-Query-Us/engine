5670b2f810fa96c9c7a8e1792409282669d11ba9
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-18 15:43:30 -0400

    good test

--- src/test/test_postlist.cpp
+++ src/test/test_postlist.cpp
@@ -6,6 +6,28 @@
 
 using namespace fast;
 
+void test(const list<uint64_t> &l) {
+  const auto space = postlist::size_needed(l);
+  auto pl = (postlist*) malloc(space);
+  memset(pl, 0, space);
+  postlist::write(l, pl);
+
+  auto it = l.begin();
+  for (const auto num : *pl) {
+    assert(*it == num);
+    ++it;
+  }
+
+  assert(it == l.end());
+
+  for (auto it = pl->begin(); it != pl->end();) {
+    const auto num = *it;
+    assert(pl->upper_bound(num) == ++it);
+  }
+
+  free(pl);
+}
+
 int main() {
   srand(0);
   list<uint64_t> l;
@@ -17,43 +39,13 @@ int main() {
     base += (rand() % 100) + 1;
   }
 
-  const auto space = postlist::size_needed(l);
+  test(l);
 
-  auto pl = (postlist*) malloc(space);
-  memset(pl, 0, space);
-
-  postlist::write(l, pl);
-
-  auto it = l.begin();
-
-  for (const auto num : *pl) {
-    assert(it != l.end());
-    assert(*it == num);
-    ++it;
-  }
-
-  for (auto it = pl->begin(); it != pl->end();) {
-    const auto s = *it;
-    assert(++it == pl->upper_bound(s));
-  }
-
-  free(pl);
 
   while (l.size() < 20'000) {
     l.push_back(base);
     base += (rand() % 100) + 1;
   }
 
-  const auto space2 = postlist::size_needed(l);
-  pl = (postlist*)malloc(space2);
-
-  memset(pl, 0, space2);
-  postlist::write(l, pl);
-
-  for (auto it = pl->begin(); it != pl->end();) {
-    const auto s = *it;
-    assert(++it == pl->upper_bound(s));
-  }
-
-  free(pl);
+  test(l);
 }
