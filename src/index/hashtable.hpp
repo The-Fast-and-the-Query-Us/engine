ef2fcb84fff9bd603da4cf86c9d555da4021f423
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-21 18:13:53 -0400

    no opt

--- src/index/hashtable.hpp
+++ src/index/hashtable.hpp
@@ -19,7 +19,7 @@ class hashtable {
     bucket(uint64_t hashval, const string_view &word) : hashval(hashval), word(word) {}
   };
 
-  size_t num_buckets, next_offset;
+  size_t num_buckets, next_offset, unique_words;
   list<bucket> *buckets;
   list<post<Doc>> docs;
 
@@ -28,7 +28,7 @@ class hashtable {
 
 public:
 
-  hashtable(size_t num_buckets = 2048) : num_buckets(num_buckets), next_offset(0) {
+  hashtable(size_t num_buckets = 2048) : num_buckets(num_buckets), next_offset(0), unique_words(0) {
     buckets = new list<bucket>[num_buckets];
   }
 
@@ -42,8 +42,10 @@ public:
 
   size_t tokens() const { return next_offset; }
 
+  size_t unique() const { return unique_words; }
+
   /*
-  * Maybe change to allow for post types?
+  * Maybe change to allow for post types? (Title vs Body)
   */
   void add(const string_view &word) {
     const auto hashval = hash(word);
@@ -58,6 +60,7 @@ public:
     }
     l.emplace_back(hashval, word);
     l.back().posts.push_back(next_offset++);
+    ++unique_words;
   }
 
   void doc_end(const string_view &url) {
