48b96c92bca36977f0760d0b5735fd56ee207805
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-26 21:23:13 -0400

    post list with iterator

--- src/index/post_list.hpp
+++ src/index/post_list.hpp
@@ -39,18 +39,61 @@ public:
 
   static unsigned char *write(const list<Offset> &posts, post_list *buffer) {
     buffer->num_words = posts.size();
-    buffer->sync_len = posts.size() / PER_SYNC;
+
+    const int SYNC_LEN = posts.size() / PER_SYNC;
+    const int POSTS_PER = posts.size() / SYNC_LEN;
+
+    buffer->sync_len = SYNC_LEN;
 
     auto wp = buffer->posts();
 
-    size_t count = 0;
+    size_t idx = 0;
     Offset last = 0;
+
     for (const auto post : posts) {
-      wp = encode(post - last, ) 
+      wp = encode(post - last, wp);
+      ++idx;
+      last = post;
+
+      if (idx % POSTS_PER == 0) {
+        buffer->sync()[idx / POSTS_PER - 1] = {size_t(wp - buffer->posts()), post};
+      }
     }
 
-    return nullptr;
+    wp = encode(0, wp);
+    buffer->len = size_t(wp - buffer->posts());
+    return wp;
   }
+
+  class isr {
+    friend class post_list;
+
+    Offset acc;
+    const unsigned char *buff;
+
+    isr(Offset base, const unsigned char *buff) : acc(base), buff(buff) {}
+
+  public:
+
+    Offset operator*() const { return acc; }
+
+    isr& operator++() {
+      uint64_t tmp;
+      buff = decode(tmp, buff);
+      acc += tmp;
+      return *this;
+    }
+  };
+
+  isr begin() const {
+    isr ans(0, posts());
+    return ++ans;
+  }
+
+  isr end() const {
+    return isr(0, posts() + len);
+  }
+
 };
 
 }
