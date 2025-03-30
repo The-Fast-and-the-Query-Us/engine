95ad51cf11202f6033f94e5381e1f8bde06b6eb6
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-30 18:48:59 -0400

    brainstorm

--- src/query/language.hpp
+++ src/query/language.hpp
@@ -3,15 +3,11 @@
 namespace fast::query {
 
 /* BNF:
-
-<Constraint>       ::= <BaseConstraint> { <OrOp> <BaseConstraint> }
-<OrOp>             ::= 'OR' | '|' | '||'
-<BaseConstraint>   ::= <SimpleConstaint> { [ <AndOp> ] <SimpleConstraint> }
-<AndOp>            ::= 'AND' | '&' | '&&'
-<SimpleConstraint> ::= <Phrase> | <NestedConstraint> | <UnaryOp> <SimpleConstraint> | <SearchWord>
-<UnaryOp>          ::= '+' | '-' | 'NOT'
-<Phrase>           ::= '"' { <SearchWord> } '"'
-<NestedConstraint> ::= '(' <Constraint> ')'
+  Requirements
+  1) words
+  2) Phrases
+  3) AND, OR, NOT
+  4) Parenthesis
 
 */
 
