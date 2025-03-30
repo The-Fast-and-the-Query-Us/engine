58ffdef174406f4ca84931da8b8c3f885c8bc067
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-30 16:04:49 -0400

    query build

---
+++ src/query/query.cpp
@@ -0,0 +1,7 @@
+namespace fast::query {
+
+void handle(const int client_fd, const int num_chunks, const char *index_dir, char *dir_end) {
+  (void)client_fd, (void)num_chunks, (void)index_dir, (void)dir_end; // TODO
+}
+
+}
