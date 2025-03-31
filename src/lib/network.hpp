b7a00f3e92d0963eb9a9c1815467dd3c9f89d777
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-31 16:20:39 -0400

    sending helpers

--- src/lib/network.hpp
+++ src/lib/network.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "string.hpp"
 #include <cstdint>
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -19,6 +20,13 @@ inline bool send_all(const int fd, const char *data, size_t len) {
 inline bool send_all(const int fd, uint32_t number) {
   number = htonl(number);
   return send_all(fd, reinterpret_cast<const char *>(&number), sizeof(number));
+}
+
+// send string as <length><data>
+inline bool send_all(const int fd, const string &str) {
+  if (!send_all(fd, str.size())) return false;
+  if (!send_all(fd, str.begin(), str.size())) return false;
+  return true;
 }
 
 }
