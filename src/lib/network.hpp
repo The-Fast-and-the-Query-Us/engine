c1352d861d507e4e504030fcca231e2a3bb9c1e7
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-31 16:42:26 -0400

    yah

--- src/lib/network.hpp
+++ src/lib/network.hpp
@@ -22,10 +22,24 @@ inline bool send_all(const int fd, uint32_t number) {
   return send_all(fd, reinterpret_cast<const char *>(&number), sizeof(number));
 }
 
-// send string as <length><data>
+// send string as <len><data>
 inline bool send_all(const int fd, const string &str) {
   if (!send_all(fd, str.size())) return false;
   if (!send_all(fd, str.begin(), str.size())) return false;
+  return true;
+}
+
+inline bool recv_all(const int fd, uint32_t &number) {
+  if (recv(fd, &number, sizeof(number), MSG_WAITALL) != sizeof(number)) return false;
+  number = ntohl(number);
+  return true;
+}
+
+inline bool recv_all(const int fd, string &str) {
+  uint32_t str_len;
+  if (!recv_all(fd, str_len)) return false;
+  str.resize(str_len);
+  if (recv(fd, str.begin(), str_len, MSG_WAITALL) == -1) return false;
   return true;
 }
 
