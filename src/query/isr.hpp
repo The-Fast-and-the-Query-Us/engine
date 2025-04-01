6401d1d94d0cddcb2d277e48ff2694310f813a7e
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-01 16:43:30 -0400

    use vector and helpers

--- src/query/isr.hpp
+++ src/query/isr.hpp
@@ -3,6 +3,7 @@
 #include <hashtable.hpp>
 #include <post_list.hpp>
 #include <isr.hpp>
+#include <vector.hpp>
 
 namespace fast::query {
 
@@ -14,21 +15,20 @@ namespace fast::query {
 * bool is_end()
 */
 
-// add phrase, OR, and AND
 class isr_or : public isr {
 
-  isr **streams;
-  size_t count;
-
+  vector<isr*> streams;
   public:
 
-  isr_or(isr **streams, size_t count) : streams(streams), count(count) {}
+  void add_stream(isr *stream) {
+    streams.push_back(stream);
+  }
 
   void next() override {
     Offset small = MAX_OFFSET;
     size_t small_idx;
 
-    for (auto i = 0u; i < count; ++i) {
+    for (auto i = 0u; i < streams.size(); ++i) {
       if (streams[i]->is_end()) continue;
 
       const auto this_offset = streams[i]->offset();
@@ -42,46 +42,48 @@ class isr_or : public isr {
   }
 
   void seek(Offset offset) override {
-    for (auto i = 0u; i < count; ++i) {
-      streams[i]->seek(offset);
+    for (auto s : streams) {
+      s->seek(offset);
     }
   }
 
   Offset offset() override {
     auto ans = MAX_OFFSET;
-    for (auto i = 0u; i < count; ++i) {
-      if (!streams[i]->is_end()) ans = min(ans, streams[i]->offset());
+    for (auto s : streams) {
+      if (!s->is_end()) ans = min(ans, s->offset());
     }
     return ans;
   }
   
   bool is_end() override {
-    for (auto i = 0u; i < count; ++i) {
-      if (!streams[i]->is_end()) return false;
+    for (auto s : streams) {
+      if (!s->is_end()) return false;
     }
     return true;
   }
 
   ~isr_or() override {
-    delete []streams;
+    for (const auto stream : streams) delete stream;
   }
 };
 
-// use this for AND and NOT
+// use this for AND and NOT (seek 0 to init)
 class isr_container : public isr {
-  isr **include, **exclude;
-  unsigned count_inc, count_ex;
+  vector<isr*> include, exclude;
   isr_doc *doc_end;
 
   public:
 
-  isr_container(isr **include, isr **exclude, unsigned count_inc, unsigned count_ex, isr_doc *doc_end) :
-    include(include), exclude(exclude), count_inc(count_inc), count_ex(count_ex), doc_end(doc_end) {
-    seek(0);
+  void add_stream(isr *stream, bool ex = false) {
+    if (ex) {
+      exclude.push_back(stream);
+    } else {
+      include.push_back(stream);
+    }
   }
 
   // jumps to next document
-  void next() override {
+  void next() override { // maybe wrong
     seek(doc_end->offset()); 
   }
 
@@ -89,9 +91,9 @@ class isr_container : public isr {
         
     // 1. Seek all the included ISRs to the first occurrence beginning at
     // the target location.
-    for (auto i = 0u; i < count_inc; ++i) {
-      include[i]->seek(offset);
-      if (include[i]->is_end()) return;
+    for (auto &stream : include) {
+      stream->seek(offset);
+      if (stream->is_end()) return;
     }
 
     while (1) {
@@ -99,44 +101,42 @@ class isr_container : public isr {
       // 2. Move the document end ISR to just past the furthest
       // contained ISR, then calculate the document begin location.
       Offset farthest = 0;
-      for (auto i = 0u; i < count_inc; ++i) {
-        farthest = max(farthest, include[i]->offset());
+      for (auto stream : include) {
+        farthest = max(farthest, stream->offset());
       }
-
       doc_end->seek(farthest + 1);
       if (doc_end->is_end()) return;
       const auto doc_begin = doc_end->offset() - doc_end->len();
 
-
       // 3. Seek all the other contained terms to past the document begin.
       bool good = true;
-      for (auto i = 0u; i < count_inc && good; ++i) {
-        include[i]->seek(doc_begin);
+      for (auto stream : include) {
+        stream->seek(doc_begin);
 
         // 5. If any ISR reaches the end, there is no match.
-        if (include[i]->is_end()) return;
+        if (stream->is_end()) return;
 
         // 4. If any contained erm is past the document end, return to
         // step 2.
-        if (include[i]->offset() > doc_end->offset()) good = false;
+        if (stream->offset() > doc_end->offset()) good = false;
       }
+
+      if (good) break;
     }
 
-    const auto doc_begin = doc_end->offset() - doc_end->len();
-    //
     // 6. Seek all the excluded ISRs to the first occurrence beginning at
     // the document begin location.
-    for (auto i = 0u; i < count_ex; ++i) {
-      if (exclude[i]->is_end()) continue;
+    const auto doc_begin = doc_end->offset() - doc_end->len();
+    for (auto stream : exclude) {
+      if (stream->is_end()) continue;
 
-      exclude[i]->seek(doc_begin);
+      stream->seek(doc_begin);
 
       // 7. If any excluded ISR falls within the document, reset the
       // target to one past the end of the document and return to
       // step 1.
-      if (exclude[i]->offset() < doc_end->offset()) {
+      if (stream->offset() < doc_end->offset()) {
         return seek(doc_end->offset());
-
       }
     }
   }
@@ -146,28 +146,28 @@ class isr_container : public isr {
   }
 
   bool is_end() override {
-    for (auto i = 0u; i < count_inc; ++i) {
-      if (include[i]->is_end()) return true;
+    for (auto stream : include) {
+      if (stream->is_end()) return true;
     }
     return false;
   }
 
   ~isr_container() override {
-    delete []include;
-    delete []exclude;
+    for (auto stream : include) delete stream;
+    for (auto stream : exclude) delete stream;
     delete doc_end;
   }
 };
 
+// call seek 0 to init
 class isr_phrase : public isr {
 
-  isr **streams;
-  size_t count;
+  vector<isr*> streams;
 
   public:
 
-  isr_phrase(isr **streams, size_t count) : streams(streams), count(count) {
-    next();
+  void add_stream(isr *stream) {
+    streams.push_back(stream);
   }
 
   void next() override {
@@ -175,20 +175,20 @@ class isr_phrase : public isr {
   }
 
   void seek(Offset offset) override {
-    for (auto i = 0u; i < count; ++i) {
-      streams[i]->seek(offset);
+    for (auto stream : streams) {
+      stream->seek(offset);
     }
 
-    while (!streams[count - 1]->is_end()) {
-      const auto base = streams[count - 1]->offset();
+    while (!streams.back()->is_end()) {
+      const auto base = streams.back()->offset();
       bool good = true;
-      for (auto i = 0u; i < count - 1 && good; ++i) {
-        const auto goal = base - count + i + 1;
+      for (auto i = 0u; i < streams.size() - 1 && good; ++i) {
+        const auto goal = base - streams.size() + i + 1;
         streams[i]->seek(goal);
 
         if (streams[i]->is_end()) return;
         else if (streams[i]->offset() != goal) {
-          streams[count - 1]->next();
+          streams.back()->next();
           good = false;
         }
       }
@@ -202,14 +202,14 @@ class isr_phrase : public isr {
   }
 
   bool is_end() override {
-    for (auto i = 0u; i < count; ++i) {
-      if (streams[i]->is_end()) return true;
+    for (auto stream : streams) {
+      if (stream->is_end()) return true;
     }
     return false;
   }
 
   ~isr_phrase() override {
-    delete []streams;
+    for (auto stream : streams) delete stream;
   }
 };
 
