4ba79a65e4d7156e44c06787d8ae3d4ea04bce6e
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-01 17:23:42 -0400

    phrase test

---
+++ src/test/test_isr_phrase.cpp
@@ -0,0 +1,52 @@
+#include <cassert>
+#include <cstdlib>
+#include <isr.hpp>
+#include <post_list.hpp>
+#include <../query/isr.hpp>
+
+const int MAX = 10'000;
+
+int main() {
+  srand(0);
+  fast::list<fast::Offset> phrases;
+
+  fast::list<fast::Offset> lists[3];
+
+  fast::Offset base = 0;
+  for (auto i = 0; i < MAX; ++i) {
+    if (rand() % 10 == 0) {
+      phrases.push_back(base);
+      lists[0].push_back(base++);
+      lists[1].push_back(base++);
+      lists[2].push_back(base++);
+    } else {
+      lists[2].push_back(base++);
+      lists[1].push_back(base++);
+      lists[0].push_back(base++);
+    }
+  }
+
+  fast::post_list *pls[3];
+  for (int i = 0; i < 3; ++i) {
+    pls[i] = (fast::post_list*) malloc(fast::post_list::size_needed(lists[i]));
+    fast::post_list::write(lists[i], pls[i]);
+  }
+
+  fast::query::isr_phrase ands;
+  for (int i = 0; i < 3; ++i) {
+    ands.add_stream(pls[i]->get_isr());
+  }
+
+  ands.seek(0); // init
+  for (const auto num : phrases) {
+    assert(!ands.is_end());
+    assert(ands.offset() == num);
+    ands.next();
+  }
+
+  assert(ands.is_end());
+
+  for (int i = 0; i < 3; ++i) {
+    free(pls[i]);
+  }
+}
