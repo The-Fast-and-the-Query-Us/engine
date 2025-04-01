0be9f25cdf3e5e1c23bc91e4682c5bde5da7b389
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-31 21:08:55 -0400

    remove old

--- src/query/isr.hpp
+++ src/query/isr.hpp
@@ -5,41 +5,5 @@
 
 namespace fast::query {
 
-class isr {
-public:
-  virtual void next();
-  virtual void next_document();
-  virtual void seek(Offset offset);
-  virtual Offset offset();
-  virtual bool end();
-};
-
-// figure out which methods we need
-class isr_or : public isr {
-  isr **terms;
-  unsigned term_count;
-
-public:
-
-  void next() override {
-
-  }
-
-  void next_document() override {
-
-  }
-
-  void seek(Offset offset) override {
-
-  }
-
-  Offset offset() override {
-
-  }
-
-  bool end() override {
-
-  }
-};
-
+// add phrase, OR, and AND
 }
