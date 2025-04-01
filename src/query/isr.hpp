868ae27ecd6a89b4dca871ac25d5995f08d268af
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-01 09:56:43 -0400

    finish isr_or

--- src/query/isr.hpp
+++ src/query/isr.hpp
@@ -17,33 +17,58 @@ namespace fast::query {
 // add phrase, OR, and AND
 class isr_or : public isr {
 
-  isr *stream;
+  isr **streams;
   size_t count;
 
   public:
 
-  void next() override {
+  isr_or(isr **streams, size_t count) : streams(streams), count(count) {}
 
+  void next() override {
+    Offset small = MAX_OFFSET;
+    size_t small_idx;
+
+    for (auto i = 0u; i < count; ++i) {
+      if (streams[i]->is_end()) continue;
+
+      const auto this_offset = streams[i]->offset();
+      if (this_offset < small) {
+        small = this_offset;
+        small_idx = i;
+      }
+    }
+
+    streams[small_idx]->next();
   }
 
   void seek(Offset offset) override {
-
+    for (auto i = 0u; i < count; ++i) {
+      streams[i]->seek(offset);
+    }
   }
 
   Offset offset() override {
-    return 0;
+    auto ans = MAX_OFFSET;
+    for (auto i = 0u; i < count; ++i) {
+      if (!is_end()) ans = min(ans, streams[i]->offset());
+    }
+    return ans;
   }
   
   bool is_end() override {
-
+    for (auto i = 0u; i < count; ++i) {
+      if (!streams[i]->is_end()) return false;
+    }
+    return true;
   }
 };
 
 class isr_and : public isr {
-
+  public:
 };
 
 class isr_phrase : public isr {
+
   isr *stream;
   size_t count;
 
