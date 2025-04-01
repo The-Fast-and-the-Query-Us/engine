4ee5e699912a5d25e1ce811904e86076fd9994e9
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-03-31 21:15:34 -0400

    extra isrs

--- src/query/isr.hpp
+++ src/query/isr.hpp
@@ -2,8 +2,21 @@
 
 #include <hashtable.hpp>
 #include <post_list.hpp>
+#include <isr.hpp>
 
 namespace fast::query {
 
 // add phrase, OR, and AND
+class isr_or : public isr {
+
+};
+
+class isr_and : public isr {
+
+};
+
+class isr_phrase : public isr {
+
+};
+
 }
