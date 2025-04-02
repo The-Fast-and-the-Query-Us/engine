243593e5960d4463a7b110412e0a41a65da952db
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-02 14:43:41 -0400

    crawl_script.py

--- mock_crawl/crawl_script.py
+++ mock_crawl/crawl_script.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from urllib.parse import urljoin
+from bloom_filter import BloomFilter
+import queue
 
-def get_html(url):
+def get_html(url: str) -> str | None:
     """Fetch HTML content of a URL."""
     try:
         response = requests.get(url, timeout=5)
@@ -12,7 +15,7 @@ def get_html(url):
         print(f"Error fetching {url}: {e}")
         return None
 
-def extract_words_and_links(html):
+def extract_words_and_links(html: str, base_url: str):
     """Extract words and links from HTML."""
     soup = BeautifulSoup(html, "html.parser")
 
@@ -24,24 +27,27 @@ def extract_words_and_links(html):
     words = re.findall(r'\b\w+\b', text)  # Extract words
 
     # Extract all links
-    links = {a['href'] for a in soup.find_all("a", href=True)}
+    links = {urljoin(base_url, a['href']) for a in soup.find_all("a", href=True)}
 
     return words, links
 
-def crawl_urls(urls):
-    """Process a list of URLs."""
-    for url in urls:
-        print(f"Fetching {url}...")
-        html = get_html(url)
-        if html:
-            words, links = extract_words_and_links(html)
-            print(f"Found {len(words)} words and {len(links)} links in {url}")
-            print("Sample Links:", list(links)[:5])  # Show first 5 links
 
-# List of URLs to start with
+MAX_QUEUE = 500
+q = queue.Queue()
+bloom = BloomFilter(max_elements=1000000, error_rate=0.01)
+
 start_urls = [
-    "https://amazon.com",
-    "https://www.python.org"
+    "https://www.nytimes.com",
 ]
 
-crawl_urls(start_urls)
+# initialize crawl
+for url in start_urls:
+    bloom.add(url)
+    q.put(url)
+
+# run crawl
+while q.qsize() > 0:
+    url = q.get()
+    html = get_html(url)
+    if html is not None:
+        words, links = extract_words_and_links(html, url)
