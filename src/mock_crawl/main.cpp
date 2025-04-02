79b16a10f5a2753036fb2c34e6ae01ccfd2b8f58
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-02 15:27:38 -0400

    working crawl storage

--- src/mock_crawl/main.cpp
+++ src/mock_crawl/main.cpp
@@ -3,12 +3,48 @@
 #include <cstring>
 #include <hashtable.hpp>
 #include <iostream>
+#include <string>
+#include <sys/fcntl.h>
+#include <hashblob.hpp>
+#include <sys/mman.h>
+#include <unistd.h>
 
 char path[5000];
 char *path_end;
 
+// max hashtable size
+constexpr int MAX_SZ = 50'000;
+
 void write(const fast::hashtable *ht) {
   static int next = 0;
+  sprintf(path_end, "%d", next);
+  ++next;
+
+  const int fd = open(path, O_CREAT | O_RDWR, 0777);
+  if (fd == -1) {
+    perror("File open fail");
+    exit(1);
+  }
+
+  const auto space = fast::hashblob::size_needed(*ht);
+
+  if (ftruncate(fd, space) == -1) {
+    perror("resize failed");
+    exit(1);
+  }
+
+  auto mptr = mmap(NULL, space, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (mptr == MAP_FAILED) {
+    perror("cant mmap");
+    close(fd);
+    exit(1);
+  }
+
+  auto blob = (fast::hashblob*) mptr;
+  fast::hashblob::write(*ht, blob);
+
+  munmap(mptr, space);
+  close(fd);
 }
 
 int main(int argc, char **argv) {
@@ -21,9 +57,26 @@ int main(int argc, char **argv) {
 
   int in;
   while (std::cin >> in) {
+    std::string word;
+    std::cin >> word;
 
+    if (in == 0) { // url
+      ht->add_doc(word.c_str());
+
+      if (ht->tokens() >= MAX_SZ) {
+        write(ht);
+        delete ht;
+        ht = new fast::hashtable;
+      }
+
+    } else if (in == 1) { // word
+      ht->add(word.c_str());
+    }
   }
 
-  write(ht);
+  if (ht->tokens() > 0) {
+    write(ht);
+  }
+
   delete ht;
 }
