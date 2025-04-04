53258bd141b70be7c366581807ef892fa90951b4
Jim Xiong <xiong.jim131@gmail.com>
2025-04-04 14:02:42 -0400

    fadsfas

--- src/lib/vector.hpp
+++ src/lib/vector.hpp
@@ -6,7 +6,7 @@
 
 #include <cassert>
 #include <common.hpp>
-#include <cstddef> // for size_t
+#include <cstddef>  // for size_t
 #include <cstdlib>
 #include <new>
 
@@ -14,12 +14,12 @@ namespace fast {
 
 template <typename T>
 class vector {
-public:
+ public:
   // Default Constructor
   // REQUIRES: Nothing
   // MODIFIES: *this
   // EFFECTS: Constructs an empty vector with capacity 0
-  vector() : elts{nullptr},  size_{0}, cap_{0} {}
+  vector() : elts{nullptr}, size_{0}, cap_{0} {}
 
   ~vector() {
     clear();
@@ -28,27 +28,23 @@ public:
 
   vector(size_t num_elements) : size_{num_elements} {
     grow(num_elements);
-    for (auto i = 0u; i < num_elements; ++i)
-      new (elts + i) T();
+    for (auto i = 0u; i < num_elements; ++i) new (elts + i) T();
   }
 
   vector(size_t num_elements, const T &val) : size_{num_elements} {
     grow(num_elements);
-    for (auto i = 0u; i < num_elements; ++i)
-      new (elts + i) T(val);
+    for (auto i = 0u; i < num_elements; ++i) new (elts + i) T(val);
   }
 
   vector(const vector<T> &other) : size_{other.size_} {
     grow(other.size_);
-    for (auto i = 0u; i < size_; ++i)
-      new (elts + i) T(other.elts[i]);
+    for (auto i = 0u; i < size_; ++i) new (elts + i) T(other.elts[i]);
   }
 
   vector operator=(const vector<T> &other) {
     if (this != &other) {
       clear();
-      if (other.size_ > cap_)
-        grow(other.size_);
+      if (other.size_ > cap_) grow(other.size_);
       for (auto i = 0u; i < other.size_; ++i) {
         new (elts + i) T(other.elts[i]);
       }
@@ -77,8 +73,7 @@ public:
   }
 
   void reserve(size_t newCapacity) {
-    if (newCapacity > cap_)
-      grow(newCapacity);
+    if (newCapacity > cap_) grow(newCapacity);
   }
 
   size_t size() const { return size_; }
@@ -91,6 +86,8 @@ public:
 
   T &back() { return elts[size_ - 1]; }
 
+  T &front() { return elts[0]; }
+
   void push_back(const T &x) {
     if (size_ == cap_) {
       grow(max(size_t(8), cap_ << 1));
@@ -100,14 +97,12 @@ public:
 
   template <typename... Args>
   void emplace_back(Args &&...args) {
-    if (size_ == cap_)
-      grow(max(size_t(8), cap_ << 1));
+    if (size_ == cap_) grow(max(size_t(8), cap_ << 1));
     new (elts + size_++) T(args...);
   }
 
   void pop_back(size_t count = 1) {
-    for (auto i = 1u; i <= count; ++i)
-      elts[size_ - i].~T();
+    for (auto i = 1u; i <= count; ++i) elts[size_ - i].~T();
     size_ -= count;
   }
 
@@ -116,12 +111,11 @@ public:
   T *end() { return elts + size_; }
 
   void clear() {
-    for (auto i = 0u; i < size_; ++i)
-      elts[i].~T();
+    for (auto i = 0u; i < size_; ++i) elts[i].~T();
     size_ = 0;
   }
 
-private:
+ private:
   T *elts = nullptr;
   size_t size_ = 0;
   size_t cap_ = 0;
@@ -132,4 +126,4 @@ private:
     cap_ = new_cap;
   }
 };
-} // namespace fast
+}  // namespace fast
