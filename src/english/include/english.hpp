68df6dc623c905b4b0c42337123d6e5649de266a
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-05 16:16:39 -0400

    todo comments

--- src/english/include/english.hpp
+++ src/english/include/english.hpp
@@ -4,10 +4,12 @@
 
 namespace fast::english {
 
+// Todo
 void trim_puntuation(fast::string &word);
 
 void porter_stem(fast::string &word);
 
+// Todo
 bool is_stop_word(const fast::string &word);
 
 }
