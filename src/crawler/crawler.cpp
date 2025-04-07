2a1ec740f377886c71488a67ce8aa5b5289976df
Conner Rose <conner.n.rose@gmail.com>
2025-04-06 20:47:08 -0400

    Add crawler.cpp

---
+++ src/crawler/crawler.cpp
@@ -0,0 +1,23 @@
+#include "crawler.hpp"
+#include <sys/signal.h>
+#include <csignal>
+
+fast::crawler::crawler c{};
+
+void handle_interrupt(int sig) {
+  if (sig == SIGINT || sig == SIGSTOP) {
+    std::cout << "graceful shutdown...\n";
+    c.shutdown();
+  }
+}
+
+int main() {
+  struct sigaction sa{};
+  memset(&sa, 0, sizeof(sa));
+  sa.sa_handler = handle_interrupt;
+  sigaction(SIGINT, &sa, nullptr);
+  sigaction(SIGSTOP, &sa, nullptr);
+
+  c.run();
+  return 0;
+}
