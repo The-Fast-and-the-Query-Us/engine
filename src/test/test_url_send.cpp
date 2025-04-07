78e4107928f4bc0ff2194a6518dff79ca5bf095c
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-07 17:41:19 -0400

    url_test

--- src/test/test_url_send.cpp
+++ src/test/test_url_send.cpp
@@ -1,8 +1,23 @@
+#include <atomic>
 #include <string.hpp>
 #include <url_sender.hpp>
 
 using namespace fast::crawler;
 
-int main() {
+const fast::string ips = "test_data/loopback_ip.txt";
+const fast::string urls[] = {"www,google.com", "www.github.com", "www.walmart.com"};
 
+std::atomic_int32_t idx = 0;
+
+void test(const fast::string &url) {
+  const auto tid = idx.fetch_add(1);
+  assert(url == urls[tid]);
+}
+
+
+int main() {
+  url_sender sender(ips, test);
+  for (const auto &url : urls) {
+    sender.send_link(url);
+  }
 }
