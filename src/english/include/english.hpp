719f3f6b33670ad510139983848e642eaa5cbbe6
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-08 11:06:41 -0400

    header

--- src/english/include/english.hpp
+++ src/english/include/english.hpp
@@ -4,10 +4,19 @@
 
 namespace fast::english {
 
+// not done yet
 void trim_puntuation(fast::string &word);
 
+// using 1980s original algorithm
+// should probably add modern optimizations before using
 void porter_stem(fast::string &word);
 
+void normalize_text(fast::string &word);
+
+// not done yet
+bool is_word(const fast::string &word);
+
+// not done yet
 bool is_stop_word(const fast::string &word);
 
 }
