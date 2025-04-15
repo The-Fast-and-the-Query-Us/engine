abd5b601d9c1297f07fd8fd523bfa2ac69e786e0
Bobby Palmer <bobbyp@umich.edu>
2025-04-15 03:56:36 +0000

    before copy

--- gcp_init.sh
+++ gcp_init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+username=$(whoami)
+sed -i "s/<USERNAME>/$username/g" "crawler.service"
+
 sudo snap install cmake --classic
 sudo apt update
 sudo apt install g++-10 make libssl-dev -y
 sudo cp ./crawler.service /etc/systemd/system/crawler.service
 sudo systemctl daemon-reload
-
-username=$(whoami)
-sed -i "s/<USERNAME>/$username/g" "crawler.service"
 
 curl -fsSL https://bun.sh/install | bash
