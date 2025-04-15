adf80cdc4174a63541f48ec289f6fcc4799354ae
Bobby Palmer <bobbyp@umich.edu>
2025-04-15 03:41:33 +0000

    auto gen gcp_init

--- gcp_init.sh
+++ gcp_init.sh
@@ -5,4 +5,8 @@ sudo apt update
 sudo apt install g++-10 make libssl-dev -y
 sudo cp ./crawler.service /etc/systemd/system/crawler.service
 sudo systemctl daemon-reload
+
+username=$(whoami)
+sed -i "s/<USERNAME>/$username/g" "crawler.service"
+
 curl -fsSL https://bun.sh/install | bash
