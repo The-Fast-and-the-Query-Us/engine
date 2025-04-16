6488676c8f4be1ab29c8602a81f9ee45a6a1800d
Conner Rose <conner.n.rose@gmail.com>
2025-04-16 18:28:29 -0400

    Stop services before starting

--- send_cmd.sh
+++ send_cmd.sh
@@ -5,6 +5,7 @@ for instance in  "$@"; do
   git@github.com:The-Fast-and-the-Query-Us/engine.git || true) && cd engine &&\
     git restore . && git checkout main && git pull && ./gcp_init.sh &&\
     ./reset_index.sh && ./frontend_init.sh && ./build.sh &&\
+  sudo systemctl stop crawler.service && sudo systemctl stop visibility.service &&\
   sudo systemctl start crawler.service && sudo systemctl start visibility.service &&\
   sudo systemctl status crawler.service && sudo systemctl status visibility.service"
 
