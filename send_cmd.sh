80355ea90158bb4459a3461676bcffddcb4971af
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-16 17:35:21 -0400

    build

--- send_cmd.sh
+++ send_cmd.sh
@@ -2,6 +2,7 @@ for instance in  "$@"; do
 
   echo "Starting $instance"
   ssh "$instance" "cd engine && git restore . && git checkout main && git pull\
-  && ./gcp_init.sh && ./reset_index.sh && ./frontend_init.sh && sudo systemctl start crawler.service"
+  && ./gcp_init.sh && ./reset_index.sh && ./frontend_init.sh && ./build.sh &&\
+  sudo systemctl start crawler.service"
 
 done
