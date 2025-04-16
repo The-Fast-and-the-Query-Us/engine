6d818361cf235aa4fda3293a068d45cfbd0f9eb1
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-16 17:57:48 -0400

    good

--- send_cmd.sh
+++ send_cmd.sh
@@ -1,8 +1,11 @@
 for instance in  "$@"; do
 
   echo "Starting $instance"
-  ssh "$instance" "cd engine && git restore . && git checkout main && git pull\
-  && ./gcp_init.sh && ./reset_index.sh && ./frontend_init.sh && ./build.sh &&\
-  sudo systemctl start crawler.service && sudo systemctl stop crawler.service"
+  ssh "$instance" "(git clone\
+  git@github.com:The-Fast-and-the-Query-Us/engine.git || true) && cd engine &&\
+    git restore . && git checkout main && git pull && ./gcp_init.sh &&\
+    ./reset_index.sh && ./frontend_init.sh && ./build.sh &&\
+  sudo systemctl start crawler.service && sudo systemctl start visibility.service &&\
+  sudo systemctl status crawler.service && sudo systemctl status visibility.service"
 
 done
