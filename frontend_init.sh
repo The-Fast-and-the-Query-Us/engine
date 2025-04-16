224aaa736fb36bf92de3c7be0043aba9953d9ab0
Bobby Palmer <bobbyp@umich.edu>
2025-04-16 18:54:58 +0000

    frontedn

--- frontend_init.sh
+++ frontend_init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt install unzip
+curl -fsSL https://bun.sh/install | bash
+source ~/.bashrc
 bun install
 bun build --outdir . --target browser frontend/js/index.tsx
 cp frontend/static/* .
