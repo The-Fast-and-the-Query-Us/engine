db332fb4e73b65158504536964ff8467f916bb87
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-16 17:12:12 -0400

    fix

--- frontend_init.sh
+++ frontend_init.sh
@@ -3,9 +3,8 @@
 mkdir -p frontend/build/
 sudo apt install unzip -y
 curl -fsSL https://bun.sh/install | bash
-source ~/.bashrc
-bun install
-export PATH=~/.bun/bin:$PATH
-bun build --outdir frontend/build --target browser frontend/js/index.tsx
+BUN="$HOME/.bun/bin/bun"
+$BUN install
+$BUN build --outdir frontend/build --target browser frontend/js/index.tsx
 cp frontend/html/* frontend/build/
 cp frontend/img/* frontend/build/
