57d53472aa2c50d062cc7b3f91cfb387a0b4404a
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-16 16:49:20 -0400

    fix script

--- send_cmd.sh
+++ send_cmd.sh
@@ -1,6 +1,10 @@
 local instances=(@a)
 
 for instance in  "${instances}"; do
+
   echo "Starting $instance"
-  ssh "$instance" "cd crawler && git pull && ./gcp_init && ./reset_index && ./frontend_init && sudo systemctl start crawler.service"
+  ssh "$instance" "cd engine && git restore . && git checkout main && git pull\
+  && ./gcp_init && ./reset_index && ./frontend_init && sudo systemctl start
+  crawler.service"
+
 done
