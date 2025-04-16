983a0d4a6da629e2f5e811880de458922f16d980
Bobby Palmer <bobbyp@umich.edu>
2025-04-16 18:55:20 +0000

    no install

--- gcp_init.sh
+++ gcp_init.sh
@@ -5,4 +5,3 @@ sudo apt update
 sudo apt install g++-10 make libssl-dev -y
 sudo cp ./crawler.service /etc/systemd/system/crawler.service
 sudo systemctl daemon-reload
-curl -fsSL https://bun.sh/install | bash
