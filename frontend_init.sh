71654e656b434b5abaf58ce74512deae598abb94
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-20 11:55:07 -0400

    frontend init

---
+++ frontend_init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo systemctl stop frontend.service
+
+cd src
+cmake -B build
+cd build
+make server
+cd ../..
+
+sudo cp systemd/frontend.service /etc/systemd/system/
+
+username=$(whoami)
+sed -i "s/<USERNAME>/$username/g" /etc/systemd/system/frontend.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable frontend.service
+sudo systemctl start frontend.service
+sudo systemctl status frontend.service
