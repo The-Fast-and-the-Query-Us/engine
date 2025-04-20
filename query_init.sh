0f85c836700f907fac1c37a17100ae8b4add3a9b
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-19 21:18:12 -0400

    query init

---
+++ query_init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo systemctl stop query_server.service
+
+cd src
+cmake -B build
+cd build
+make query_server
+cd ../..
+
+sudo cp systemd/query_server.service /etc/systemd/system/
+
+username=$(whoami)
+sed -i "s/<USERNAME>/$username/g" /etc/systemd/system/query_server.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable query_server.service
+sudo systemctl start query_server.service
+sudo systemctl status query_server.service
