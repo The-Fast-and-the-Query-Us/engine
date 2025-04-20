7fc4264b8d23d2bc9967f12c688d99f499ed5e9b
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-19 21:56:18 -0400

    more

--- frontend_init.sh
+++ frontend_init.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-mkdir -p frontend/build/
-sudo apt install unzip -y
-curl -fsSL https://bun.sh/install | bash
-BUN="$HOME/.bun/bin/bun"
-$BUN install
-$BUN build --outdir frontend/build --target browser frontend/js/index.tsx
-cp frontend/html/* frontend/build/
-cp frontend/img/* frontend/build/
+cd src
+cmake -B build
+cd build
+make server
+cd ../..
+
