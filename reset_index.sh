5bae682fe584fe5ea845f605befd2a8d83e25377
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-19 21:49:34 -0400

    safety

--- reset_index.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf ~/.local/share/crawler/
-mkdir -p ~/.local/share/crawler/index
-touch ~/.local/share/crawler/docs.log
-# cp ~/engine/mock_crawl/init.txt ~/.local/share/crawler/seed_list.txt
-
-echo "index reset!"
