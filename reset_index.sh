45a4d0040e22cafc25dfbe2ff0d9a33776adcf8a
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-26 18:31:19 -0400

    yer

--- reset_index.sh
+++ reset_index.sh
@@ -2,4 +2,4 @@
 
 rm -rf ~/.local/share/crawler/
 mkdir -p ~/.local/share/crawler/index
-cp ~/engine/mock_crawl/init.txt ~/.local/share/crawler/seed_list.txt
+#cp ~/engine/mock_crawl/init.txt ~/.local/share/crawler/seed_list.txt
